42f4e1f3ffdd301a05edaf12251c96328830689f
Axel de Arias <ADEARIAS@MacBook-Pro-de-soportelocal.local>
2022-01-17 01:08:42 -0300

    rename function

--- src/components/TaskSummary.tsx
+++ src/components/TaskSummary.tsx
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet, StyleProp, ViewStyle } from 'react-native'
 import { TaskItem } from '../domain/Tasks/TaskModel'
 import useTasksService from '../domain/Tasks/useTaskService'
 
-const filterByState = (state: TaskItem['state']) => (task: TaskItem) => {
+const byState = (state: TaskItem['state']) => (task: TaskItem) => {
   return task.state === state
 }
 
@@ -17,9 +17,9 @@ type TaskSummaryProps = {
 const TaskSummary = ({ containerStyle }: TaskSummaryProps) => {
   const { tasks } = useTasksService()
 
-  const hoursCompleted = tasks.filter(filterByState('COMPLETED')).reduce(sumEstimate, 0)
-  const hoursInProgress = tasks.filter(filterByState('IN_PROGRESS')).reduce(sumEstimate, 0)
-  const hoursPlanned = tasks.filter(filterByState('PLANNED')).reduce(sumEstimate, 0)
+  const hoursCompleted = tasks.filter(byState('COMPLETED')).reduce(sumEstimate, 0)
+  const hoursInProgress = tasks.filter(byState('IN_PROGRESS')).reduce(sumEstimate, 0)
+  const hoursPlanned = tasks.filter(byState('PLANNED')).reduce(sumEstimate, 0)
 
   return (
     <View style={containerStyle}>
